f99c2da69f9c04e73a7fddcc64539c462df62c59
oyajun <81346541+oyajun@users.noreply.github.com>
2023-08-11 22:29:22 +0900

    [update]同時押し可能に

--- lf310.cpp
+++ lf310.cpp
@@ -12,10 +12,6 @@
 void LF310::ParseHIDData(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
     if (HIDUniversal::VID != LF310_VID || HIDUniversal::PID != LF310_PID) // Make sure the right device is actually connected
         return;
-    memcpy(&lf310Data, buf, min(len, MFK_CASTUINT8T sizeof(lf310Data)));    
-    static LF310DataButtons oldButtonState;
-    if (lf310Data.btn.val != oldButtonState.val) { // Check if anything has changed
-        buttonClickState.val = lf310Data.btn.val & ~oldButtonState.val; // Update click state variable
-        oldButtonState.val = lf310Data.btn.val;
-    }
+    memcpy(&lf310Data, buf, min(len, MFK_CASTUINT8T sizeof(lf310Data)));
+    buttonClickState.val = lf310Data.btn.val
 }
